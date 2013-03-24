6ebdef25ec4f5e4b2a7301be6a40cf408274b00f
AndrewProLee <me@andrewleenj.com>
2013-03-23 23:09:54 -0400

    Fixed a bug with editing posts.

--- canvas/models/forms/poster.php
+++ canvas/models/forms/poster.php
@@ -20,38 +20,31 @@ use \Wires\Database\DB as DB;
 use \Wires\Routing\URI as URI;
 
 class Poster {
+	const TOPIC = 1;
+	const POST = 2;
+	const EDIT = 3;
+
 	//Returns the post form type.
 	public static function getType(){
 		$uri = new URI();
 
-		if($uri->getArg(0) == 'post'){
-			if(!is_null($uri->getArg(1)) && !is_null($uri->getArg(2))){
-				if($uri->getArg(1) == 'post' && !is_null(Canvas::getTopic(Canvas::getID()))){
-					return 'post';
-				}
-				else if($uri->getArg(1) == 'topic' && !is_null(Canvas::getForum(Canvas::getID()))){
-					return 'topic';
-				}
-				else if($uri->getArg(1) == 'edit'){
-					if(!is_null(Canvas::getTopic($uri->getArg(2)))){
-						if(!is_null($uri->getArg(3))){
-							if(!is_null(Canvas::getPost($uri->getArg(3)))){
-								return 'edit';
-							}
-						}
-					}
-				}
+		if(preg_match('#post/(((post|topic)/\d{6})|(edit(/\d{6}){2}))#', $uri->getURI())){
+			if($uri->getArg(1) == 'topic'){
+				return Canvas::getForum() ? static::TOPIC : -1;
+			}
+			else if($uri->getArg(1) == 'post'){
+				return Canvas::getTopic() ? static::POST : -1;
+			}
+			else if($uri->getArg(1) == 'edit'){
+				return Canvas::getTopic($uri->getArg(2)) && Canvas::getPost($uri->getArg(3)) ? static::EDIT : -1;
 			}
 		}
-		else if($uri->getArg(0) == 'topic'){
-			return 'post';
-		}
 		
-		return false;
+		return -1;
 	}
 
 	public static function getPost(){
-		if(static::getType() == 'edit'){
+		if(static::getType() == static::EDIT){
 			$uri = new URI();
 			return Canvas::getPost($uri->getArg(3));
 		}
@@ -61,7 +54,7 @@ class Poster {
 	}
 
 	public static function getTopic(){
-		if(static::getType() == 'edit'){
+		if(static::getType() == static::EDIT){
 			$uri = new URI();
 			return Canvas::getTopic($uri->getArg(2));
 		}
@@ -72,7 +65,7 @@ class Poster {
 
 	public static function post(){
 		if(!empty($_POST) && $_SERVER['REQUEST_METHOD'] == 'POST'){
-			if(static::getType() == 'topic'){
+			if(static::getType() == static::TOPIC){
 				if(empty($_POST['name'])){
 					Canvas::logError('You must specify a topic name');
 				}
@@ -101,10 +94,10 @@ class Poster {
 					return static::newPost($tid);
 				}
 			}
-			else if(static::getType() == 'post'){
+			else if(static::getType() == static::POST){
 				return static::newPost(Canvas::getID());
 			}
-			else if(static::getType() == 'edit'){
+			else if(static::getType() == static::EDIT){
 				$uri = new URI();
 
 				return static::editPost($uri->getArg(2), $uri->getArg(3));
@@ -117,7 +110,7 @@ class Poster {
 	//Creates a new post.
 	public static function newPost($tid){
 		if(empty($_POST['contents']) || strlen(trim($_POST['contents'])) < Settings::getSetting('minPostLength')){
-			Canvas::logError('You post must contain at least ' . Settings::getSetting('minPostLength') . ' characters');
+			Canvas::logError('Your post must contain at least ' . Settings::getSetting('minPostLength') . ' characters');
 		}
 
 		if(!count(Canvas::getErrors())){
